7864a7714e2747755b818c978f087b5d66bcfc96
Bernadette B <90285283+beebubs@users.noreply.github.com>
2021-12-23 15:47:59 -0800

    San Diego Top Spots

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
 // write your code here
 
+$.getJSON("data.json", function(data){
+	console.log(data); 
+	//var a = $('<a />')
+	//a.attr('href', www.google.com/maps/search/?api=1&query);
+	//a.text(desiredText);
+	//$('body').append(a + "%2C" + );
+	
+  $(data).each(function(i, topSpots){
+	let googleMaps = ("https://www.google.com/maps/search/?api=1&query=" + topSpots.location[0] + "%2C" + topSpots.location[1])
+  	$('#topSpotsBody').append($("<tr>")
+        .append($("<td>").append(topSpots.name))
+        .append($("<td>").append(topSpots.description))
+		.append($("<td>").append(`<a href=${googleMaps}>Open In Google Maps</a>`))); 
+		
+  });
+})
+
 });
